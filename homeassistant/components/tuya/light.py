17cd12c3d0e5a0141177b04ef7682c6a6aecf53c
Warwick Davison <34164294+Waz-Cpt@users.noreply.github.com>
2022-03-03 10:51:52 +0200

    Update light.py
    This has been tested onmy home assistant instance by cloning into custom components. It could be neatened up as the integration picks up channel one as light."modules name" and channel two is light."module name"_light.

--- homeassistant/components/tuya/light.py
+++ homeassistant/components/tuya/light.py
@@ -110,8 +110,9 @@ LIGHTS: dict[str, tuple[TuyaLightEntityDescription, ...]] = {
         ),
         # Not documented
         # Based on multiple reports: manufacturer customized Dimmer 2 switches
+        # The scond channel requires key=DPCpde.SWITCH_1 and not SWITCH_LED_1
         TuyaLightEntityDescription(
-            key=DPCode.SWITCH_LED_1,
+            key=DPCode.SWITCH_1,
             name="Light",
             brightness=DPCode.BRIGHT_VALUE_1,
         ),
